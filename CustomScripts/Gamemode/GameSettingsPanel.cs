79855e09554dfc25c157e2f712042b96bfeee2e3
Frityet <ambhogal01@gmail.com>
2021-10-05 08:46:21 -0700

    GameSettingsPanel.cs

---
+++ CustomScripts/Gamemode/GameSettingsPanel.cs
@@ -0,0 +1,17 @@
+using System;
+using FistVR;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace CustomScripts
+{
+    public class GameSettingsPanel : MonoBehaviour
+    {
+        //TODO: Set the OnClick actions
+        public FVRPointableButton LimitedAmmo;
+        public FVRPointableButton MoreEnemies;
+        public FVRPointableButton FasterEnemies;
+        public FVRPointableButton WeakerEnemies;
+    }
+}
